e669395cd07e854e3ac4e8384487d8ce8e5f8dc0
flxti <flxx@protonmail.com>
2023-01-03 14:54:27 +0100

    Change font

--- config.h
+++ config.h
@@ -4,7 +4,8 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+    "Font Awesome:size=18"
+//    "Source Code Pro:Regular:size=10:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -22,4 +23,4 @@ static unsigned int lines      = 4;
  */
 static const char worddelimiters[] = " ";
 
-static unsigned int border_width = 0;
+static unsigned int border_width = 1;
